1bfe06cc1791ff8c0206d4d3bef9fe6e1d5ccda4
RickySiamtora <vicious.pbb@gmail.com>
2025-12-25 00:10:31 +0900

    fix: simplify start learning card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.bisayaspeak.ai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 41
-        versionName = "1.0.41"
+        versionCode = 42
+        versionName = "1.0.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
